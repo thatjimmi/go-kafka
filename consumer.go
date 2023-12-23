36a9c5b1a40ce83aef8a3aa4aa0fb7323c4d7dde
Jimmi Mortensen <52856973+thatjimmi@users.noreply.github.com>
2023-12-23 21:12:21 +0100

    Postgres

--- consumer.go
+++ consumer.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/lib/pq"
 )
 
 func RunConsumer() {
+	db := connectDB()
+	defer db.Close()
+
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
 		"group.id":          "myGroup",
@@ -25,9 +31,22 @@ func RunConsumer() {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+			_, err := db.Exec("INSERT INTO messages (message) VALUES ($1)", string(msg.Value))
+			if err != nil {
+				log.Fatal(err)
+			}
 		} else {
 			// The client will automatically try to recover from all errors.
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
+}
+
+func connectDB() *sql.DB {
+	connStr := "postgres://myuser:mypassword@localhost:5433/mydb?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/thatjimmi/go-kafka
 
 go 1.19
 
-require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2
+require github.com/confluentinc/confluent-kafka-go v1.9.2
 
-)
+require github.com/lib/pq v1.10.9 // indirect
